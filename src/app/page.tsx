8d34139bba575befac5eeabbbd37264010c4c7d1
joaovrcosta <joaovr.costa@gmail.com>
2025-01-28 11:24:07 -0300

    feat: added Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,29 @@
-import Image from "next/image";
+import { ChevronRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex items-center justify-center ">
+      <div className="max-w-[1216px]">
+        <div className="flex flex-col items-center space-y-8 mt-14">
+          <h1 className="text-5xl/[65px] text-center">
+            Se especialize como desenvolvedor Front-end e{" "}
+            <span className="font-bold bg-blue-gradient-second bg-clip-text text-transparent">
+              conquiste vagas nacionais e internacionais
+            </span>
+          </h1>
+          <div className="max-w-[756px] text-center">
+            <p>
+              A plataforma completa pra você aprender programação do zero no seu
+              ritmo, se tornar Full Stack e se especializar em diversas
+              tecnologias.
+            </p>
+          </div>
+          <button className="bg-blue-gradient h-[52px] rounded-full border border-[#25252A] text-sm flex items-center justify-center px-6">
+            INSCREVA-SE AGORA
+            <ChevronRight />
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import codeLegendsLogo from "../../public/code-legends-logo.svg";
+import { ChevronRight, User } from "lucide-react";
+import circleIcon from "../../public/circle.svg";
+
+export default function Header() {
+  return (
+    <div className="relative fixed top-0 left-0 w-full z-50 bg-[#121214]">
+      <ul className="flex justify-between items-center py-6 px-16">
+        <li>
+          <Image src={codeLegendsLogo} alt="Code Legends" />
+        </li>
+        <li className="flex space-x-12 items-center">
+          <ul className="flex space-x-24 items-center">
+            <li className="flex">
+              <a
+                href=""
+                className="p-3 flex space-x-2 hover:bg-[#202024] rounded-xl hover:text-white px-8"
+              >
+                <Image src={circleIcon} alt="" />
+                <span className="text-sm text-[#c4c4cc]">
+                  Conteúdos gratuitos
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="p-3 hover:bg-[#202024] rounded-xl hover:text-white px-8"
+              >
+                <span className="text-sm text-[#c4c4cc]">Cursos</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="p-3 hover:bg-[#202024] rounded-xl hover:text-white px-8"
+              >
+                <span className="text-sm text-[#c4c4cc]">Contato</span>
+              </a>
+            </li>
+          </ul>
+          <ul className="flex items-center space-x-4 border-l-2 border-[#29292e]">
+            <li className="ml-4">
+              <a
+                href=""
+                className="p-3 flex space-x-2 text-[#c4c4cc] px-8 hover:bg-[#202024] rounded-xl hover:text-white"
+              >
+                <User size={20} />
+                <span className="text-sm ">LOGIN</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="p-3 px-9 flex bg-blue-gradient rounded-full border border-[#25252A] items-center space-x-2"
+              >
+                <span className="font-semibold text-sm">INSCREVA-SE</span>
+                <ChevronRight />
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+}
